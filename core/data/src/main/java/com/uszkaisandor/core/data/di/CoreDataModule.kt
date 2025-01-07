f7e280f595ae5e000d47b46a3bc57a90afdcd4d5
Sándor Uszkai <uszkaisanyi@gmail.com>
2025-01-07 17:36:34 +0100

    Add token refresh mechanism

--- core/data/src/main/java/com/uszkaisandor/core/data/di/CoreDataModule.kt
+++ core/data/src/main/java/com/uszkaisandor/core/data/di/CoreDataModule.kt
@@ -1,10 +1,15 @@
 package com.uszkaisandor.core.data.di
 
+import com.uszkaisandor.core.data.auth.EncryptedSessionStorage
 import com.uszkaisandor.core.data.networking.HttpClientFactory
+import com.uszkaisandor.core.domain.SessionStorage
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreDataModule = module {
     single {
         HttpClientFactory().build()
     }
+    singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
 }
--- core/domain/src/main/java/com/uszkaisandor/core/domain/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.uszkaisandor.core.domain
-
-class MyClass {
-}
