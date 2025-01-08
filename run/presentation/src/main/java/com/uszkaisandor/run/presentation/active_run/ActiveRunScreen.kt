e8e39b7fd51f946783fc58534a609de0ae37d64d
Sándor Uszkai <uszkaisanyi@gmail.com>
2025-01-08 13:23:23 +0100

    Add permission handling

--- run/presentation/src/main/java/com/uszkaisandor/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/uszkaisandor/run/presentation/active_run/ActiveRunScreen.kt
@@ -2,6 +2,13 @@
 
 package com.uszkaisandor.run.presentation.active_run
 
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,18 +17,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.uszkaisandor.core.presentation.designsystem.RuniqueTheme
 import com.uszkaisandor.core.presentation.designsystem.StartIcon
 import com.uszkaisandor.core.presentation.designsystem.StopIcon
+import com.uszkaisandor.core.presentation.designsystem.components.RuniqueDialog
 import com.uszkaisandor.core.presentation.designsystem.components.RuniqueFloatingActionButton
+import com.uszkaisandor.core.presentation.designsystem.components.RuniqueOutlinedActionButton
 import com.uszkaisandor.core.presentation.designsystem.components.RuniqueScaffold
 import com.uszkaisandor.core.presentation.designsystem.components.RuniqueToolbar
 import com.uszkaisandor.run.presentation.R
 import com.uszkaisandor.run.presentation.active_run.components.RunDataCard
+import com.uszkaisandor.run.presentation.util.hasLocationPermission
+import com.uszkaisandor.run.presentation.util.hasNotificationPermission
+import com.uszkaisandor.run.presentation.util.shouldShowLocationPermissionRationale
+import com.uszkaisandor.run.presentation.util.shouldShowNotificationPermissionRationale
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -34,11 +49,63 @@ fun ActiveRunScreenRoot(
     )
 }
 
+
 @Composable
 private fun ActiveRunScreen(
     state: ActiveRunState,
     onAction: (ActiveRunAction) -> Unit
 ) {
+    val context = LocalContext.current
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { perms ->
+        val hasCourseLocationPermission = perms[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+        val hasFineLocationPermission = perms[Manifest.permission.ACCESS_FINE_LOCATION] == true
+        val hasNotificationPermission = if (Build.VERSION.SDK_INT >= 33) {
+            perms[Manifest.permission.POST_NOTIFICATIONS] == true
+        } else true
+
+        val activity = context as ComponentActivity
+        val showLocationRationale = activity.shouldShowLocationPermissionRationale()
+        val showNotificationRationale = activity.shouldShowNotificationPermissionRationale()
+
+        onAction(
+            ActiveRunAction.SubmitLocationPermissionInfo(
+                acceptedLocationPermission = hasCourseLocationPermission && hasFineLocationPermission,
+                showLocationRationale = showLocationRationale
+            )
+        )
+        onAction(
+            ActiveRunAction.SubmitNotificationPermissionInfo(
+                acceptedNotificationPermission = hasNotificationPermission,
+                showNotificationRationale = showNotificationRationale
+            )
+        )
+    }
+
+    LaunchedEffect(key1 = true) {
+        val activity = context as ComponentActivity
+        val showLocationRationale = activity.shouldShowLocationPermissionRationale()
+        val showNotificationRationale = activity.shouldShowNotificationPermissionRationale()
+
+        onAction(
+            ActiveRunAction.SubmitLocationPermissionInfo(
+                acceptedLocationPermission = context.hasLocationPermission(),
+                showLocationRationale = showLocationRationale
+            )
+        )
+        onAction(
+            ActiveRunAction.SubmitNotificationPermissionInfo(
+                acceptedNotificationPermission = context.hasNotificationPermission(),
+                showNotificationRationale = showNotificationRationale
+            )
+        )
+
+        if (!showLocationRationale && !showNotificationRationale) {
+            permissionLauncher.requestRuniquePermissions(context)
+        }
+    }
+
     RuniqueScaffold(
         withGradient = false,
         topAppBar = {
@@ -52,14 +119,20 @@ private fun ActiveRunScreen(
         },
         floatingActionButton = {
             RuniqueFloatingActionButton(
-                icon = if (state.shouldTrack) StopIcon else StartIcon,
+                icon = if (state.shouldTrack) {
+                    StopIcon
+                } else {
+                    StartIcon
+                },
                 onClick = {
                     onAction(ActiveRunAction.OnToggleRunClick)
                 },
                 iconSize = 20.dp,
-                contentDescription = stringResource(
-                    id = if (state.shouldTrack) R.string.pause_run else R.string.start_run
-                )
+                contentDescription = if (state.shouldTrack) {
+                    stringResource(id = R.string.pause_run)
+                } else {
+                    stringResource(id = R.string.start_run)
+                }
             )
         }
     ) { padding ->
@@ -77,6 +150,60 @@ private fun ActiveRunScreen(
                     .fillMaxWidth()
             )
         }
+    }
+
+    if (state.showLocationRationale || state.showNotificationRationale) {
+        RuniqueDialog(
+            title = stringResource(id = R.string.permission_required),
+            onDismiss = { /* Normal dismissing not allowed for permissions */ },
+            description = when {
+                state.showLocationRationale && state.showNotificationRationale -> {
+                    stringResource(id = R.string.location_notification_rationale)
+                }
+
+                state.showLocationRationale -> {
+                    stringResource(id = R.string.location_rationale)
+                }
+
+                else -> {
+                    stringResource(id = R.string.notification_rationale)
+                }
+            },
+            primaryButton = {
+                RuniqueOutlinedActionButton(
+                    text = stringResource(id = R.string.okay),
+                    isLoading = false,
+                    onClick = {
+                        onAction(ActiveRunAction.DismissRationaleDialog)
+                        permissionLauncher.requestRuniquePermissions(context)
+                    }
+                )
+            }
+        )
+    }
+}
+
+private fun ActivityResultLauncher<Array<String>>.requestRuniquePermissions(
+    context: Context
+) {
+    val hasLocationPermission = context.hasLocationPermission()
+    val hasNotificationPermission = context.hasNotificationPermission()
+
+    val locationPermissions = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+    )
+    val notificationPermission = if (Build.VERSION.SDK_INT >= 33) {
+        arrayOf(Manifest.permission.POST_NOTIFICATIONS)
+    } else arrayOf()
+
+    when {
+        !hasLocationPermission && !hasNotificationPermission -> {
+            launch(locationPermissions + notificationPermission)
+        }
+
+        !hasLocationPermission -> launch(locationPermissions)
+        !hasNotificationPermission -> launch(notificationPermission)
     }
 }
 
--- run/presentation/src/main/java/com/uszkaisandor/run/presentation/active_run/ActiveRunViewModel.kt
+++ run/presentation/src/main/java/com/uszkaisandor/run/presentation/active_run/ActiveRunViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 
 class ActiveRunViewModel : ViewModel() {
@@ -15,7 +16,32 @@ class ActiveRunViewModel : ViewModel() {
     private val eventChannel = Channel<ActiveRunEvent>()
     val events = eventChannel.receiveAsFlow()
 
-    fun onAction(action: ActiveRunAction) {
+    private val _hasLocationPermission = MutableStateFlow(false)
+    private val _hasNotificationPermission = MutableStateFlow(false)
 
+    fun onAction(action: ActiveRunAction) {
+        when (action) {
+            ActiveRunAction.OnFinishRunClick -> {}
+            ActiveRunAction.OnResumeRunClick -> {}
+            ActiveRunAction.OnToggleRunClick -> {}
+            is ActiveRunAction.SubmitLocationPermissionInfo -> {
+                _hasLocationPermission.value = action.acceptedLocationPermission
+                state = state.copy(showLocationRationale = true)
+            }
+
+            is ActiveRunAction.SubmitNotificationPermissionInfo -> {
+                _hasNotificationPermission.value = action.acceptedNotificationPermission
+                state = state.copy(showNotificationRationale = true)
+            }
+
+            ActiveRunAction.DismissRationaleDialog -> {
+                state = state.copy(
+                    showNotificationRationale = false,
+                    showLocationRationale = false
+                )
+            }
+
+            ActiveRunAction.OnBackClick -> {}
+        }
     }
 }
